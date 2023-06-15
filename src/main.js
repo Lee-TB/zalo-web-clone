88510e8d732041c61761cab8ab38f8849edb59a1
Lee TB <tblyduc2412@gmail.com>
2023-06-15 10:13:04 +0700

    migrate to typesciprt

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import './assets/main.css'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import '@/assets/main.css';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import { createApp } from 'vue';
+import App from '@/App.vue';
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
