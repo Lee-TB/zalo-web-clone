bce6dc3569f99140d5a651e92156ef89f4867b38
IndieDev <tblyduc2412@gmail.com>
2023-07-14 18:00:47 +0700

    setup: app use pinia

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from '@/App.vue';
 
 // These things below are global, you need to import them into .storybook/preview.ts
 import '@/assets/css/main.css';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia);
+app.mount('#app');
